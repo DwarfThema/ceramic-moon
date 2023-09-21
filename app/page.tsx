9a5e748883fad7ffb4acbf5613f5fc929191d575
Junho Park <86091261+DwarfThema@users.noreply.github.com>
2023-09-21 16:43:42 +0900

    Dev (#3)
    * merge_fps / moonScene
    
    * merge_fps, moonScene
    
    * player_fix_position
    
    * ceramics_implement flying ceramics
    
    * erase unsed hdr
    
    * change hdr name
    
    * effect_modify bloom value
    
    * moon_modify position:

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,19 @@
 "use client";
 
-import { Environment, KeyboardControls } from "@react-three/drei";
+import {
+  Effects,
+  Environment,
+  KeyboardControls,
+  Stats,
+} from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import MainScene from "./src/mainScene";
 import { Suspense } from "react";
+import { Bloom, EffectComposer, Vignette } from "@react-three/postprocessing";
 
 export default function Home() {
   return (
-    <main className="flex h-screen w-screen flex-col items-center justify-between bg-white">
+    <main className="flex h-screen w-screen flex-col items-center justify-between">
       <KeyboardControls
         map={[
           { name: "forward", keys: ["ArrowUp", "w", "W"] },
@@ -18,19 +24,21 @@ export default function Home() {
           { name: "sit", keys: ["v"] },
         ]}
       >
+        <Stats />
         <Canvas shadows camera={{ fov: 40 }}>
-          <Environment
-            files={"/textures/hdr.hdr"}
-            background
-            ground={{
-              height: -100,
-              radius: 10,
-              scale: 0,
-            }}
-          />
+          <Environment files={"/textures/hdr.hdr"} background blur={0.4} />
+          <fog attach="fog" args={["#202030", 10, 5000]} />
           <Suspense fallback={null}>
             <MainScene />
           </Suspense>
+          <EffectComposer>
+            <Vignette eskil={false} offset={0.05} darkness={0.7} />
+            <Bloom
+              luminanceThreshold={0.5}
+              luminanceSmoothing={3}
+              intensity={2}
+            />
+          </EffectComposer>
         </Canvas>
       </KeyboardControls>
     </main>
---
+++ app/src/star.tsx
@@ -0,0 +1,34 @@
+import { PointMaterial, Points } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { random } from "maath";
+import { useRef, useState } from "react";
+
+export default function Stars({ ...props }) {
+  const ref = useRef(null) as any;
+  const [sphere] = useState(() =>
+    random.inSphere(new Float32Array(5000), { radius: 1.5 })
+  );
+  useFrame((state, delta) => {
+    ref.current.rotation.x -= delta / 10;
+    ref.current.rotation.y -= delta / 15;
+  });
+  return (
+    <group rotation={[0, 0, Math.PI / 4]}>
+      <Points
+        ref={ref}
+        positions={sphere as any}
+        stride={3}
+        frustumCulled={false}
+        {...props}
+      >
+        <PointMaterial
+          transparent
+          color="#ffa0e0"
+          size={0.005}
+          sizeAttenuation={true}
+          depthWrite={false}
+        />
+      </Points>
+    </group>
+  );
+}
