72a942addd9b8b0e5f160ab021ae0a89c016b729
Junho Park <86091261+DwarfThema@users.noreply.github.com>
2023-10-04 20:59:20 +0900

    Dev (#14)
    * merge_fps / moonScene
    
    * merge_fps, moonScene
    
    * player_fix_position
    
    * ceramics_implement flying ceramics
    
    * erase unsed hdr
    
    * change hdr name
    
    * effect_modify bloom value
    
    * moon_modify position:
    
    * ceramic_add moon ceramic / shadow
    
    * fix_shadow
    
    * mobile_setting camera
    
    * mobile_add polar Max angle
    
    * mobile_camera position
    
    * ceramics_add back cermaics
    
    * erase useControl
    
    * add LoadingScreen
    
    * rabbit
    
    * set title
    
    * loading_interactive
    
    * fix_LoadingPage
    
    * mobile_fix 100vh
    
    * loadingScreen_apply percentage

--- app/src/loadingScreen.tsx
+++ app/src/loadingScreen.tsx
@@ -3,26 +3,33 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 
 export default function LoadingScreen() {
-  const { progress } = useProgress();
+  const { progress, loaded } = useProgress();
   const [loading, setLoading] = useState(false);
-  const [opacity, setOpacity] = useState(0);
+  const [transitionEnd, setTransitionEnd] = useState(false);
 
   useEffect(() => {
-    if (progress >= 100) {
+    console.log(loaded);
+    if (loaded >= 28) {
       setLoading(true);
+      setTimeout(() => {
+        setTransitionEnd(true);
+      }, 5500);
     }
-  }, [progress]);
+  }, [progress, loaded]);
 
   return (
     <div
       className={clsstail(
-        "transition-opacity ease-in-out duration-[5000ms] absolute bg-[#252525] w-screen h-screen flex flex-col justify-center items-center z-20 text-[#DDDDDD] ",
-        loading ? "opacity-0" : "opacity-100"
+        "transition-opacity ease-in-out duration-[5000ms] absolute bg-[#252525] w-screen h-screen flex flex-col justify-center items-center text-[#DDDDDD] ",
+        loading ? "opacity-0" : "opacity-100",
+        transitionEnd ? "z-0 hidden" : "z-20"
       )}
     >
-      <div className="xl:pt-36 pt-64">LOADING</div>
-      <div className="font-extrabold mb-5">{Math.floor(progress)}%</div>
-      <div className="xl:w-[23%] w-[55%] h-[50%]  bg-no-repeat bg-contain bg-[url('/Logo.svg')]" />
+      <div className="xl:pt-36 pt-[50%]">LOADING</div>
+      <div className="font-extrabold mb-5">
+        {Math.floor((loaded / 28) * 100)}%
+      </div>
+      <div className="xl:w-[23%] w-[43%] h-[50%]  bg-no-repeat bg-contain bg-[url('/Logo.svg')]" />
     </div>
   );
 }
