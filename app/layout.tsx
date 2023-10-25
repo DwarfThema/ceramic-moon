f84f2385fc1077b69281206a8d0d53862894313d
Junho Park <dwarfthema@gmail.com>
2023-10-25 17:43:57 +0900

    meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,10 +5,50 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://www.dalmagi.co.kr/"),
   title: "Dal-Ma-Gi",
-  description: "VIVLE-THEME-MUSEUM : Dal Ma Gi Project",
+  description: "THEME-MUSEUM : Dal-Ma-Gi by Donggi Eun",
+  openGraph: {
+    title: "Dal-Ma-Gi",
+    description: "THEME-MUSEUM : Dal-Ma-Gi by Donggi Eun",
+    url: "https://www.dalmagi.co.kr/",
+    images: [
+      {
+        url: "/thumbnail.jpeg",
+        width: 800,
+        height: 600,
+      },
+      {
+        url: "/thumbnail.jpeg",
+        width: 1800,
+        height: 1600,
+        alt: "My custom alt",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "https://www.dalmagi.co.kr/",
+    description: "THEME-MUSEUM : Dal-Ma-Gi by Donggi Eun",
+    creator: "Donggi Eun & Junho Park",
+    images: ["/thumbnail.jpeg"],
+  },
+  robots: {
+    index: false,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
 };
-
 export default function RootLayout({
   children,
 }: {
