dfe2e7fd62ccb6c21746e3b256315869383ad8c7
Junho Park <dwarfthema@gmail.com>
2023-10-30 10:19:44 +0900

    merge dev

--- app/src/ceramics.tsx
+++ app/src/ceramics.tsx
@@ -5,11 +5,7 @@ import { useEffect, useMemo, useRef, useState } from "react";
 import { Group, Material, Mesh } from "three";
 
 export default function Ceramics() {
-<<<<<<< HEAD
-  const { scene } = useGLTF("/models/cermaics.glb");
-=======
   const { scene } = useGLTF("/models/cermaics.gltf");
->>>>>>> dev
   const ceramicScene = useMemo(() => scene, [scene]);
   const { nodes } = useGraph(ceramicScene);
   const ceramicMesh = nodes.Scene as Group;
@@ -36,36 +32,14 @@ export default function Ceramics() {
           (pos.position[2] < 0 || pos.position[2] > 10)
       );
   }, []);
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-  const { posx, posy, posz, scale } = useControls({
-    posx: { value: 31.5, step: 0.1 },
-    posy: { value: 56, step: 1 },
-    posz: { value: -9, step: 1 },
-    scale: { value: 59, step: 1 },
-  });
-
-=======
->>>>>>> 72a942addd9b8b0e5f160ab021ae0a89c016b729
-=======
->>>>>>> dev
   const positionsB = useMemo(() => {
     return [...Array(60)]
       .map(() => ({
         position: [
-<<<<<<< HEAD
-          posx - Math.random() * scale,
-          posy - Math.random() * scale,
-          posz - Math.random() * scale,
-=======
           31.5 - Math.random() * 59,
           56 - Math.random() * 59,
           -9 - Math.random() * 59,
-<<<<<<< HEAD
->>>>>>> 72a942addd9b8b0e5f160ab021ae0a89c016b729
-=======
->>>>>>> dev
         ],
       }))
       .filter(
@@ -76,28 +50,7 @@ export default function Ceramics() {
       );
   }, []);
 
-<<<<<<< HEAD
-  /*   const positionsB = [...Array(60)]
-    .map(() => ({
-      position: [
-        posx - Math.random() * scale,
-        posy - Math.random() * scale,
-        posz - Math.random() * scale,
-      ],
-    }))
-    .filter(
-      (pos) =>
-        (pos.position[0] < 0 || pos.position[0] > 15) &&
-        (pos.position[1] < 0 || pos.position[1] > 15) &&
-        (pos.position[2] < 0 || pos.position[2] > 15)
-    ); */
-
-  useEffect(() => {
-    //console.log(positionsA), [];
-  });
-=======
   useEffect(() => {});
->>>>>>> dev
 
   return (
     <group>
@@ -167,28 +120,6 @@ function Ceramic({
   });
 
   return (
-<<<<<<< HEAD
-<<<<<<< HEAD
-    <Float speed={0.3} floatIntensity={0.2} floatingRange={[1, 5]}>
-      <mesh
-        ref={ref}
-        onClick={() => {
-          setClicked(true);
-          console.log("good");
-        }}
-        geometry={mesh.geometry}
-        position={[
-          positionB?.position[0] as number,
-          positionB?.position[1] as number,
-          positionB?.position[2] as number,
-        ]}
-        scale={12}
-        {...props}
-      />
-    </Float>
-=======
-=======
->>>>>>> dev
     <>
       <Float speed={0.3} floatIntensity={0.2} floatingRange={[1, 5]}>
         <mesh
@@ -225,9 +156,5 @@ function Ceramic({
         />
       </Float>
     </>
-<<<<<<< HEAD
->>>>>>> 72a942addd9b8b0e5f160ab021ae0a89c016b729
-=======
->>>>>>> dev
   );
 }
